c099e6823796e6534cc20e4cca9be3d3359d9ca3
Misuiro <113217149+MizuruAndFelix@users.noreply.github.com>
2023-06-09 15:21:05 +0300

    1

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,15 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	string name = " ";
+	cout << "Введите имя:";
+	cin >> name;
+	cout << "\nЗдравствуйте, " << name;
+	return 0;
+}
